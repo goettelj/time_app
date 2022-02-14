6d977e83806734c097f6ac30b3d6f5405689948b
Jay Goettelmann <jay@liberatedmatter.com>
2022-02-14 00:12:19 -0500

    Adds additional /time route for lab 2.

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from datetime import datetime
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return 'Hello Docker!'
+
+@app.route('/time')
+def current_time():
+    now = datetime.now()
+    return now.strftime("%I:%M%p")
 
 
 app.run(host='0.0.0.0',
